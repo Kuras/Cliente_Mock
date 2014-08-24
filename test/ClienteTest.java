31e76b47d1314f43ff4cc0cb6da48850557f2bf0
Przemek Kura <kuraprzemek@gmail.com>
2014-08-24 13:04:51 +0200

    add new test and configured Framework Mockito

---
+++ test/ClienteTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class) //MockitoAnnotations.initMocks(this)
+public class ClienteTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+		fail("Not yet implemented");
+	}
+
+}
