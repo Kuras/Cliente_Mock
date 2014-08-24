71d6ec81262e997c047bb8c19b3da236554d9a48
Przemek Kura <kuraprzemek@gmail.com>
2014-08-24 13:08:28 +0200

    add simple test for cliente class

--- test/ClienteTest.java
+++ test/ClienteTest.java
@@ -9,14 +9,12 @@ import org.mockito.runners.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class) //MockitoAnnotations.initMocks(this)
 public class ClienteTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
 
+	
 	@Test
 	public void test() {
 		
-		fail("Not yet implemented");
+		Cliente cliente = new Cliente();
 	}
 
 }
